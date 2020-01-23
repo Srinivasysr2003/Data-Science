1a9c39974d941b570417154a472c3d27c6751fe0
Srinivas Reddy Yalala <srinivasysr2003@yahoo.com>
2020-01-23 15:09:46 +0530

    First Java program

---
+++ Test1.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.List;
+public class Test1 {
+
+public static void main(String args[]) {
+
+  System.out.println("In first main()");
+
+}
+
+public static void main(char args[]) {
+
+  System.out.println('a');
+
+}
+
+}
